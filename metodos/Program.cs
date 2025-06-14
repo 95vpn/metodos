3b0e60c886e622f4f7c06c712dbcba1e9b42d967
Johnny Herrera <jmjaramillo95@gmail.com>
2025-06-14 18:20:54 -0500

    Agregar archivos de proyecto.

---
+++ metodos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new Program();
+            data.metodoPrivado();
+
+        }
+
+        private void metodoPrivado()
+        {
+            Console.WriteLine("Hola");
+            Console.ReadLine();
+        }
+    }
+}
